d6cef4c1b528bd19a84ffe3d005b99475759fd4e
yaninyzwitty <ianmwa143@gmail.com>
2026-05-21 16:22:50 +0300

    implement configuration loading and PostgreSQL connection management

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/yaninyzwitty/webhook-gateway-service/internal/config"
+	"github.com/yaninyzwitty/webhook-gateway-service/internal/postgres"
+)
 
 func main() {
-	fmt.Println("Hello, Gateway!")
+	// Load configuration
+	configPath := flag.String("config", "config.yaml", "the path to your config file")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		slog.Error("failed to load config: ", "error", err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	pool, err := postgres.New(ctx, &cfg.Postgres, postgres.WithMaxConns(100), postgres.WithMinConns(5))
+	if err != nil {
+		slog.Error("failed to connect to postgres: ", "error", err)
+		os.Exit(1)
+	}
+
+	defer pool.Close()
+
+	slog.Info("Port", "value", cfg.Server.Port)
 }
